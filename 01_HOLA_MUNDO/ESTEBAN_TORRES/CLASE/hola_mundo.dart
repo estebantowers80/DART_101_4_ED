1ea0e87ac9d10fba6799b70a48c0bf07eab44932
estebantowers80 <estebantowers@gmail.com>
2024-11-09 20:54:35 -0500

    revision clase 3

---
+++ 01_HOLA_MUNDO/ESTEBAN_TORRES/CLASE/hola_mundo.dart
@@ -0,0 +1,12 @@
+///hola mundo en Dart
+///void -> no te retorna nada ->funciones
+///mail() -> funcion principal
+///{  -> bloque de la funcion o cuerpo
+///
+///}
+///
+
+void main() {
+  /// print -> funcion u obejeto que imprime un valor
+  print("Hola Mundo, soy Esteban :D");
+}
