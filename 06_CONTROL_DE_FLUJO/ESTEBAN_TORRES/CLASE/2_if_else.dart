f3517a85111b6bb2f19875d790e7c7e09e2612fb
estebantowers80 <estebantowers@gmail.com>
2024-11-17 22:54:19 -0500

    tarea del if clase del dia 12

--- 06_CONTROL_DE_FLUJO/ESTEBAN_TORRES/CLASE/2_if_else.dart
+++ 06_CONTROL_DE_FLUJO/ESTEBAN_TORRES/CLASE/2_if_else.dart
@@ -1,12 +1,22 @@
 import "dart:io";
 
 void main(){
-  final edad=28;
+
+  final edad=14;
 
   if(edad>=18){
     print("Es mayor de edad");
   }else{
     print("Es menor de edad");
+  }
+
+
+  if( edad >= 18){
+    print("Es mayor de edad");
+  }else if ( edad < 12 ){
+    print("Es niño");
+  }else {
+    print("Es adolecente");
   }
 
   /// esto si me se del IF 
---
+++ 06_CONTROL_DE_FLUJO/ESTEBAN_TORRES/TAREA/2_tarea_edad.dart
@@ -0,0 +1,26 @@
+import "dart:developer";
+import "dart:io";
+
+void main(){
+  print("Ingrese la edad como entero:");
+
+  //el ingreso de datos y se guarda en una variable
+  final datoEdad = stdin.readLineSync() ?? '0';  // no olvidar lo del null safety poner con ??
+
+  final edad = int.tryParse(datoEdad) ?? -1;
+
+  if ( edad >= 0 && edad <= 1){
+    print("Es un bebe");
+  } else if ( edad >1 && edad<=12){
+    print("Es un niño");
+  }else if ( edad >12 && edad<=28){
+    print("Es un adolecente");
+  }else if ( edad >28 && edad<=63){
+    print("Es un adulto");
+  }else if ( edad >63){
+    print("Es un adulto mayor");
+  } else {
+    print("ingreso un dato erroneo");
+  }
+
+}
